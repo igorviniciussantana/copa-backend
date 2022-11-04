23a9a1a4c05153a0f6742f85a68d1d873c1fbab1
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2022-11-04 19:43:09 -0400

    feat(prisma): create seed

---
+++ prisma/seed.ts
@@ -0,0 +1,60 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john.doe@gmail.com",
+      avatarUrl: "https://github.com/igorviniciussantana.png",
+    },
+  });
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: "Example Pool",
+      code: "BOL123",
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-12T12:00:00.915Z",
+      firstTeamCountryCode: "DE",
+      secondTeamCountryCode: "BR",
+    },
+  });
+
+  await prisma.game.create({
+    data: {
+      date: "2022-11-12T13:00:00.915Z",
+      firstTeamCountryCode: "BR",
+      secondTeamCountryCode: "AR",
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id,
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+main();
