25093c5e4d18015605f245f8a193af66ba44d4aa
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-03-01 13:15:24 -0400

    refactor: fix Poll model name

--- src/server.ts
+++ src/server.ts
@@ -1,67 +1,61 @@
-import Fastify from 'fastify'
-import cors from '@fastify/cors'
-import { z } from 'zod'
-import ShortUniqueId from 'short-unique-id'
-import { PrismaClient } from '@prisma/client'
+import Fastify from "fastify";
+import cors from "@fastify/cors";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
-    log: ['query'],
-})
+  log: ["query"],
+});
 
-async function bootstrap(){
-const fastify = Fastify({
+async function bootstrap() {
+  const fastify = Fastify({
     logger: true,
-})
+  });
 
-await fastify.register(cors, {
-    origin: true
-})
+  await fastify.register(cors, {
+    origin: true,
+  });
 
+  fastify.get("/polls/count", async () => {
+    const count = await prisma.poll.count();
 
-fastify.get('/pools/count', async()=>{
-    const count = await prisma.pool.count()
-   
-    return {count}
-})
+    return { count };
+  });
 
-fastify.get('/users/count', async()=>{
-    const count = await prisma.user.count()
-   
-    return {count}
-})
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
 
+    return { count };
+  });
 
-fastify.get('/guesses/count', async()=>{
-    const count = await prisma.guess.count()
-   
-    return {count}
-})
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
 
+    return { count };
+  });
 
+  fastify.post("/polls", async (request, reply) => {
+    const createPollBody = z.object({
+      title: z.string(),
+    });
 
-fastify.post('/pools', async(request, reply)=>{
+    const { title } = createPollBody.parse(request.body);
 
-const createPoolBody = z.object({
-    title: z.string(),
-})
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
 
-   const { title } = createPoolBody.parse(request.body)
-
-   const generate =  new ShortUniqueId({length: 6})
-   const code = String(generate()).toUpperCase()
-
-   await prisma.pool.create({
-    data: {
+    await prisma.poll.create({
+      data: {
         title,
         code,
-    }
-   })
+      },
+    });
 
-   return reply.status(201).send({code})
-})
+    return reply.status(201).send({ code });
+  });
 
-
-await fastify.listen({port: 3333})
+  await fastify.listen({ port: 3333 });
 }
 
-bootstrap()
+bootstrap();
