a1ccf6c55cbddd039d37819988b96eac0a69b2f9
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-03-01 17:59:41 -0400

    feat: setup jwt

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -45,7 +45,18 @@ export async function authRoutes(fastify: FastifyInstance) {
         },
       });
     }
-    
+
+    const token = fastify.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: "7 days",
+      }
+    );
+
     return { userInfo };
   });
 }
