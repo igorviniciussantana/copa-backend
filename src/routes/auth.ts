cfc814d57e8c040da05fd0597b495418685782ff
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-03-01 13:48:06 -0400

    feat: setup google auth route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,7 +2,38 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
+export async function authRoutes(fastify: FastifyInstance) {
+  fastify.post("/users", async (request) => {
+    const createUserBody = z.object({
+      acess_token: z.string(),
+    });
 
-export async function authRoutes(fastify : FastifyInstance){
-   
+    const { acess_token } = createUserBody.parse(request.body);
+
+    const userResponse = await fetch(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${acess_token}`,
+        }
+      }
+    );
+
+
+    const userData = await userResponse.json();
+
+
+    const userInfoSchema = z.object({
+        id: z.string(),
+        email: z.string().email(),
+        name: z.string(),
+        picture: z.string().url()
+    })
+
+    const userInfo = userInfoSchema.parse(userData)
+
+
+    return { userInfo }
+  });
 }
