9d5e838505571920addae5ebf5b11f9928eaa0b9
igorviniciussantana <igor.santana@estudante.ifms.edu.br>
2023-03-01 14:13:52 -0400

    fix: fix jwt_secret types

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,28 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { z } from "zod";
-import jwt from '@fastify/jwt'
+import jwt from "@fastify/jwt";
 import { pollRoutes } from "./routes/polls";
 import { userRoutes } from "./routes/user";
 import { guessRoutes } from "./routes/guess";
 import { authRoutes } from "./routes/auth";
 import { gameRoutes } from "./routes/game";
 
+
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
+  const jwt_secret = process.env.JWT_SECRET || 'secret'
+
   await fastify.register(cors, {
     origin: true,
   });
 
-await fastify.register(jwt, {
-    secret: process.env.JWT_SECRET
-})
+  await fastify.register(jwt, {
+    secret: jwt_secret
+  });
 
   await fastify.register(pollRoutes);
   await fastify.register(userRoutes);
